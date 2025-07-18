ddb41f7bd5028a23508b043ee16eac9d3f1c187e
amaldev8055 <adev86952@gmail.com>
2025-07-18 12:16:07 +0530

    add category,delete user

--- routers/adminrouter.js
+++ routers/adminrouter.js
@@ -14,12 +14,14 @@ route.post('/category/add', categoryadd)
 var { Listcategory }=require('../controller/admin/addcategory')
 route.post('/category/list', Listcategory)
 
-
 var { deletecategory } =require('../controller/admin/addcategory')
 route.post('/category/delete', deletecategory)
 
 var { editcategory } =require('../controller/admin/addcategory')
 route.post('/category/edit', editcategory)
+
+var{deleteuser}=require('../controller/user/listUser')
+route.post('/delete/user',deleteuser)
 
 
 
