1e4fcd1723674017e5443a9818efd8bfee3f1a86
Rahil666 <rahilramesh666@gmail.com>
2025-07-25 13:28:24 +0530

    router

--- routers/shoprouter.js
+++ routers/shoprouter.js
@@ -2,14 +2,14 @@ var express = require("express");
 var route = express.Router();
 // var {verifyToken}=require('./components/jwt')
 
-// var { login } = require('./controller/login')
-// route.post('/login', login)
-
 var { AddShop } = require('./../controller/shop/addShop')
 route.post('/add/shop', AddShop)
 
 var { ListShops } = require('./../controller/shop/addShop')
 route.post('/list/shop', ListShops)
+
+var { DeleteShops } = require('./../controller/shop/addShop')
+route.post('/delete/shop', DeleteShops)
 
 var { editshops } = require('./../controller/shop/addShop')
 route.post('/edit/shop', editshops)
